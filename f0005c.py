83aea04145fb3c270df54d9a198bb5cf9d578567
erdeiolver <71698306+erdeiolver@users.noreply.github.com>
2020-11-03 12:30:20 +0100

    Create f0005c.py

---
+++ f0005c.py
@@ -0,0 +1,11 @@
+gondolt_szám = 4
+tipp = input('Szerinted melyik számra gondolok 1 és 5 között? ')
+tipp = int(tipp)
+gondolt_szám2 = 4
+tipp2 = input('Szerinted melyik számra gondolok 1 és 5 között? ')
+tipp2 = int(tipp2)
+if gondolt_szám2 == tipp2:
+    print('Eltaláltad!')
+    print('Te vagy az ász!')
+else:
+    print('Búj el mert ma viszik szemetett.')
